97e990ca3e378ebab50ac5cd492d5e4b45e30ca6
Eilsan <eilert.sandvand@icloud.com>
2026-07-02 21:48:41 +0200

    fwj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY >= 500) {
+    if (window.scrollY >= 170) {
         header.classList.add("smaller");
     } else {
         header.classList.remove("smaller");
